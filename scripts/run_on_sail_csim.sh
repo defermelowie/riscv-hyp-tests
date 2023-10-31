01e1b58d08f8b7d9223735ae25f17abf7ddcdfaa
Lowie Deferme <lowie.deferme@kuleuven.be>
2023-10-31 16:44:38 +0100

    add sail target platform

---
+++ scripts/run_on_sail_csim.sh
@@ -0,0 +1,14 @@
+# Run tests on C based emulator extracted from sail model
+# 
+# Assumes sail model sits next to this repository 
+# and that C based emulator is build
+
+csim=../sail-riscv/c_emulator/riscv_sim_RV64;
+
+# Build tests
+make clean;
+export PLAT=sail LOG_LEVEL=LOG_WARNING;
+make
+
+# Run tests
+$csim --ram-size 256 -t log/sail_term.log build/sail/rvh_test.elf > log/sail_trace.log
---
+++ scripts/run_on_sail_osim.sh
@@ -0,0 +1,14 @@
+# Run tests on Ocaml based emulator extracted from sail model
+# 
+# Assumes that the RISC-V sail model sits next to this 
+# repository and that Ocaml based emulator is build
+
+osim=../sail-riscv/ocaml_emulator/riscv_ocaml_sim_RV64;
+
+# Build tests
+make clean;
+export PLAT=sail LOG_LEVEL=LOG_WARNING;
+make
+
+# Run tests
+$osim -ram-size 256 -enable-hext build/sail/rvh_test.elf > log/sail_trace.log 2> log/sail_term.log
